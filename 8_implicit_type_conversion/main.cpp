5cc4ecdfb22c9effb4762f1f38f3eb5066774b4a
Taeo Kim <kweras95@gmail.com>
2023-03-27 15:07:30 +0200

    Add function templates

--- 8_implicit_type_conversion/main.cpp
+++ 8_implicit_type_conversion/main.cpp
@@ -27,6 +27,24 @@ auto max(T x, U y)
     return (x < y) ? y : x;
 }
 
+template <typename T>
+auto add(T x, T y)
+{
+    return x + y;
+}
+
+template <typename T, typename U>
+T mult(T x, U y)
+{
+    return x * y;
+}
+
+template <typename T, typename U>
+auto sub(T x, U y)
+{
+    return x - y;
+}
+
 int main()
 {
     int i{2};
@@ -71,6 +89,16 @@ int main()
     std::cout << max<double>(2, 3.5) << '\n'; // we've provided actual type double, so the compiler won't use template argument deduction
 
     std::cout << max(2, 3.5) << '\n';
+
+    std::cout << add(2, 3) << '\n';
+	std::cout << add(1.2, 3.4) << '\n';
+
+    std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+
+    std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
     
     return 0;
 }
