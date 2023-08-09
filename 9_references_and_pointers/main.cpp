4d328ed6e014ee068addb2f0c05d8d2c9e8df2d1
Taeo Kim <kweras95@gmail.com>
2023-08-09 20:15:06 +0200

    Add type deduction with pointers, refs, and const

--- 9_references_and_pointers/main.cpp
+++ 9_references_and_pointers/main.cpp
@@ -73,6 +73,9 @@ int& max(int& x, int& y)
     return (x > y) ? x : y;
 }
 
+const std::string& getConstRef(); // some function that returns a const reference
+const std::string* const getConstPtr(); // some function that returns a const pointer to a const value
+
 int main()
 {
     int x { 5 };
@@ -133,5 +136,34 @@ int main()
 
     std::cout << a << b << '\n';
 
+/*
+    const int x;    // this const applies to x, so it is top-level
+    int* const ptr; // this const applies to ptr, so it is top-level
+
+    const int& ref; // this const applies to the object being referenced, so it is low-level
+    const int* ptr; // this const applies to the object being pointed to, so it is low-level
+
+    const int* const ptr; // the left const is low-level, the right const is top-level
+
+    auto ref1{ getConstRef() };        // std::string (top-level const and reference dropped)
+    const auto ref2{ getConstRef() };  // const std::string (const reapplied, reference dropped)
+
+    auto& ref3{ getConstRef() };       // const std::string& (reference reapplied, low-level const not dropped)
+    const auto& ref4{ getConstRef() }; // const std::string& (reference and const reapplied)
+
+
+    auto ptr1{ getConstPtr() };  // const std::string*
+    auto* ptr2{ getConstPtr() }; // const std::string*
+
+    auto const ptr3{ getConstPtr() };  // const std::string* const
+    const auto ptr4{ getConstPtr() };  // const std::string* const
+
+    auto* const ptr5{ getConstPtr() }; // const std::string* const
+    const auto* ptr6{ getConstPtr() }; // const std::string*
+
+    const auto const ptr7{ getConstPtr() };  // error: const qualifer can not be applied twice
+    const auto* const ptr8{ getConstPtr() }; // const std::string* const
+
+*/
     return 0;
 }
