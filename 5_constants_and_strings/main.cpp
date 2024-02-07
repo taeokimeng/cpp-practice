f2928452934a718f3945d341224b970814bbe17a
Taeo Kim <taeokimeng@gmail.com>
2024-02-07 12:45:12 +0100

    Update string

--- 5_constants_and_strings/main.cpp
+++ 5_constants_and_strings/main.cpp
@@ -1,5 +1,7 @@
-#include <bitset> // for std::bitset
 #include <iostream>
+#include <string> // for std::string and std::getline
+#include <string_view> // C++17
+#include <bitset> // for std::bitset
 #include <format> // C++20
 // #include <print> // C++23
 
@@ -11,6 +13,14 @@ int main()
     std::cout << std::format("{:b}\n", 0b1010); // C++20
     std::cout << std::format("{:#b}\n", 0b1010); // C++20
     // std::println("{:b} {:#b}", 0b1010, 0b1010); // C++23
+
+    std::cout << "Enter your full name: ";
+    std::string name{};
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Your name is " << name << '\n';
+    std::cout << name << " has " << name.length() << " characters\n";
+    std::cout << name << " has " << std::ssize(name) << " characters\n";
 
     return 0;
 }
